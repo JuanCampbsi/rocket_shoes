a7bc365e4f618b968722b35ce80e2f7c12e922b9
Juan Campos <juancampos@iMac-de-Juan.local>
2022-01-26 14:24:25 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,9 +7,10 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart;
 
+  
   return (
     <Container>
       <Link to="/">
